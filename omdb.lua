9c86562d92a0e1eb56f456b5b77c864173b75860
Casper Koning <casper.e.koning@gmail.com>
2016-05-04 20:59:47 +0200

    Handle whitespace in query

--- omdb.lua
+++ omdb.lua
@@ -1,34 +1,55 @@
+function urlEncode(str)
+  if (str) then
+    str = string.gsub (str, "\n", "\r\n")
+    str = string.gsub (str, "([^%w %-%_%.%~])", function (c) return string.format ("%%%02X", string.byte(c)) end)
+    str = string.gsub (str, "^%s*(.-)%s*$", "%1")
+    str = string.gsub (str, " ", "+")
+  end
+  return str
+end
+
 function imdbUrl(imdbId)
   return "http://www.imdb.com/title/" .. imdbId
 end
 
-function omdbQuery(query)
-  return "http://omdbapi.com/?s=" .. query
+function omdbQuerySearch(query)
+  return "http://omdbapi.com/?s=" .. urlEncode(query)
+end
+
+function createPosterView(element, index)
+  local posterView = hs.webview.new(hs.geometry(408, 187 + 37 * (index - 1), 25, 38))
+  posterView:url(element["Poster"])
+  posterView:show()
+  return posterView
 end
 
 function searchOmdb()
   local picker = hs.chooser.new(function(userInput)
     if userInput ~= nil then
-      hs.execute("open " .. imdbUrl(userInput["imdbId"]) )
+      hs.execute("open " .. imdbUrl(userInput["imdbId"]))
     end
   end)
 
   picker:queryChangedCallback(function(query)
-    hs.http.asyncGet(omdbQuery(query), nil, function(status, body, headers)
+    hs.http.asyncGet(omdbQuerySearch(query), nil, function(status, body, headers)
       if status == 200 then
         searchResult = hs.json.decode(body)
-        local values = {}
-        hs.fnutils.each(searchResult["Search"], function(each)
-          local foundTitle = each["Title"]
-          local foundYear = each["Year"]
-          local foundImdbId = each["imdbID"]
+        local choices = {}
+        if searchResult["Search"] ~= nil then
+          hs.fnutils.each(searchResult["Search"], function(each)
+            local foundTitle = each["Title"]
+            local foundYear = each["Year"]
+            local foundImdbId = each["imdbID"]
 
-          table.insert(values, {
-            text = foundTitle .. " (" .. foundYear .. ")",
-            imdbId = foundImdbId
-          })
-        end)
-        picker:choices(values)
+            table.insert(choices, {
+              text = foundTitle .. " (" .. foundYear .. ")",
+              subText = foundPlot,
+              imdbId = foundImdbId
+            })
+          end)
+        end
+
+        picker:choices(choices)
       end
     end)
   end)
